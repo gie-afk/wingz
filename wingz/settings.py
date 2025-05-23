814db8f5375f8329eb28b374bf74e8df29140119
Rowegie Lambojon <rowegie@tracesense.com>
2025-05-23 23:38:55 +0800

    updated settings for custom user

--- wingz/settings.py
+++ wingz/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local apps
+    "apps.user",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
